3068c33946c6ec906a26e0c6b41780492352f7b5
Dongmin Jung <dmdmjung@gmail.com>
2017-07-25 23:33:36 +0000

    add testthat
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_5/madman/Rpacks/PPInfer@131576 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ tests/testthat/test_net.infer.ST.R
@@ -0,0 +1,23 @@
+
+
+test_that("net.infer.ST()", {
+  data(litG)
+  litG <- igraph.from.graphNEL(litG)
+  sg <- decompose(litG, min.vertices = 50)
+  sg <- sg[[1]]
+  K <- net.kernel(sg)
+  expect_error(net.infer.ST(names(V(sg))[1], K, top=20))
+  expect_error(net.infer.ST(names(V(sg)), K, top=20))
+  
+  Net.infer.ST <- net.infer.ST(names(V(sg))[1:10], K, top=20)
+  expect_is(Net.infer.ST, 'list')
+  
+  expect_equivalent(Net.infer.ST, net.infer.ST(names(V(sg))[1:10], K, top=20))
+  expect_equal(Net.infer.ST, net.infer.ST(names(V(sg))[1:10], K, top=20))
+  expect_identical(Net.infer.ST, net.infer.ST(names(V(sg))[1:10], K, top=20))
+  
+  expect_equal(Net.infer.ST$list, names(V(sg))[1:10])
+  expect_true(identical(intersect(net.infer.ST(names(V(sg))[1:10], K, top=10)$top, names(V(sg))[1:10]), character(0)))
+  expect_gt(length(Net.infer.ST$top), length(net.infer.ST(names(V(sg))[1:10], K, top=10)$top))
+})
+
