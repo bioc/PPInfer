a12343e91ea1545432e96b7062a4e78554bb8390
dongminjung <dongminjung@Dongmins-MacBook-Pro.local>
2017-11-28 00:15:57 +0900

    Update

--- tests/testthat/test_ORA.dotplot.R
+++ tests/testthat/test_ORA.dotplot.R
@@ -7,33 +7,32 @@ test_that("ORA.dotplot()", {
   set.seed(1)
   gene.id <- sample(geneNames, 100)
   result.ORA <- ORA(examplePathways, gene.id)
-  category <- rownames(result.ORA)
   
   plot.ORA <- tempfile("plot", fileext = ".png")
   png(filename = plot.ORA, width = 2000, height = 1600, res = 300)
-  ORA.dotplot(data.frame(category, result.ORA), category = "category", size = "Size",
+  ORA.dotplot(result.ORA, category = "Category", size = "Size",
               count = "Count", pvalue = "pvalue", sort = "pvalue")
   dev.off()
   
-  expect_error(ORA.dotplot(data.frame(category, result.ORA), size = "Size",
-                           count = "Count", pvalue = "pvalue", sort = "pvalue"))
-  expect_error(ORA.dotplot(data.frame(category, result.ORA), category = "category",
-                           count = "Count", pvalue = "pvalue", sort = "pvalue"))
-  expect_error(ORA.dotplot(data.frame(category, result.ORA), category = "category",
-                           size = "Size", pvalue = "pvalue", sort = "pvalue"))
+  expect_error(ORA.dotplot(result.ORA, size = "Size", count = "Count",
+                           pvalue = "pvalue", sort = "pvalue"))
+  expect_error(ORA.dotplot(result.ORA, category = "Category", count = "Count",
+                           pvalue = "pvalue", sort = "pvalue"))
+  expect_error(ORA.dotplot(result.ORA, category = "Category", size = "Size",
+                           pvalue = "pvalue", sort = "pvalue"))
   
-  result.table <- ORA.dotplot(data.frame(category, result.ORA), category = "category", size = "Size",
+  result.table <- ORA.dotplot(result.ORA, category = "Category", size = "Size",
                               count = "Count", pvalue = "pvalue", sort = "pvalue",
                               plot = FALSE)
   expect_is(result.table$Pvalue, 'numeric')
   
-  expect_equivalent(result.table, ORA.dotplot(data.frame(category, result.ORA), category = "category", size = "Size",
+  expect_equivalent(result.table, ORA.dotplot(result.ORA, category = "Category", size = "Size",
                                               count = "Count", pvalue = "pvalue", sort = "pvalue",
                                               plot = FALSE))
-  expect_equal(result.table, ORA.dotplot(data.frame(category, result.ORA), category = "category", size = "Size",
+  expect_equal(result.table, ORA.dotplot(result.ORA, category = "Category", size = "Size",
                                          count = "Count", pvalue = "pvalue", sort = "pvalue",
                                          plot = FALSE))
-  expect_identical(result.table, ORA.dotplot(data.frame(category, result.ORA), category = "category", size = "Size",
+  expect_identical(result.table, ORA.dotplot(result.ORA, category = "Category", size = "Size",
                                              count = "Count", pvalue = "pvalue", sort = "pvalue",
                                              plot = FALSE))
 })
